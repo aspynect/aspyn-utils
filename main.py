4f118c8632ae5eada490ac9f3da2498a3ac11de2
PistachioPiper <peppypiper14@gmail.com>
2024-09-03 13:31:18 -0500

    the great embeddening

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import random
 import d20
 
+myColor = discord.Color.from_rgb(r=255, g=0, b=255)
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
@@ -23,7 +24,9 @@ def sus(id):
 @tree.command(name="ping",description="ping")
 async def ping(interaction: discord.Interaction):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     await interaction.response.send_message("h", ephemeral = True)
 
@@ -33,7 +36,9 @@ async def ping(interaction: discord.Interaction):
 @tree.command(name="sync",description="sync")
 async def sync(interaction: discord.Interaction):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     await tree.sync()
     await interaction.response.send_message("sunk!", ephemeral = True)
@@ -46,17 +51,22 @@ async def sync(interaction: discord.Interaction):
 @app_commands.describe(game="Which UID?")
 @app_commands.choices(game=[
         app_commands.Choice(name="Genshin", value="609006374"),
-        app_commands.Choice(name="StarRail", value="604969370"),
+        app_commands.Choice(name="Honkai Star Rail", value="604969370"),
         app_commands.Choice(name="Valorant", value="aspyn#sheit"),
         app_commands.Choice(name="Steam", value="883076786"),
         app_commands.Choice(name="Bungie", value="aspyn#5311")
-    ])
-        #TODO add switch fc
+    ]
+)
+#TODO add switch fc
 async def uid(interaction: discord.Interaction, game: app_commands.Choice[str]):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
-    await interaction.response.send_message(game.value)
+    embed = discord.Embed(title = f"UID", color = myColor)
+    embed.add_field(name = game.name, value = game.value)
+    await interaction.response.send_message(embed = embed)
     
 
 
@@ -66,7 +76,9 @@ async def uid(interaction: discord.Interaction, game: app_commands.Choice[str]):
 @app_commands.describe(dicestring="Dice Expression")
 async def roll(interaction: discord.Interaction, dicestring: str):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     try:
         result = str(d20.roll(dicestring))
@@ -78,11 +90,39 @@ async def roll(interaction: discord.Interaction, dicestring: str):
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@tree.command(name="rollchar",description="Roll a Character!")
+async def rollchar(interaction: discord.Interaction):
+    if sus(interaction.user.id):
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
+        return
+    result = ""
+    for i in range(6):
+        result += f"{d20.roll("4d6rr1kh3")}\n"
+    await interaction.response.send_message(result)
+
+
+
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@tree.command(name="rollhelp",description="Link to d20 docs")
+async def rollhelp(interaction: discord.Interaction):
+    if sus(interaction.user.id):
+        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        return
+    await interaction.response.send_message("https://github.com/avrae/d20?tab=readme-ov-file#operators", ephemeral = True)
+
+
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @tree.command(name="temperature",description="Convert Temperatures")
 @app_commands.describe(input="Input amount and units (with a space)")
 async def roll(interaction: discord.Interaction, input: str):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     
     input.lower()
@@ -103,39 +143,19 @@ async def roll(interaction: discord.Interaction, input: str):
     outputString = ""
     outputString += f"{tempInF:.1f} Degrees Fahrenheit\n"
     outputString += f"{(tempInF - 32) / 1.8:.1f} Degrees Celsius\n"
-    await interaction.response.send_message(outputString, ephemeral = True)
+    embed = discord.Embed(title = "Temperature Conversion", color = myColor)
+    embed.add_field(name = "", value = outputString)
+    await interaction.response.send_message(embed = embed, ephemeral = True)
 
-
-    #await interaction.response.send_message(result)
-
-@app_commands.allowed_installs(guilds=True, users=True)
-@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-@tree.command(name="rollchar",description="Roll a Character!")
-async def rollchar(interaction: discord.Interaction):
-    if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
-        return
-    result = ""
-    for i in range(6):
-        result += f"{d20.roll("4d6rr1kh3")}\n"
-    await interaction.response.send_message(result)
-
-
-@app_commands.allowed_installs(guilds=True, users=True)
-@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
-@tree.command(name="rollhelp",description="Link to d20 docs")
-async def rollhelp(interaction: discord.Interaction):
-    if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
-        return
-    await interaction.response.send_message("https://github.com/avrae/d20?tab=readme-ov-file#operators", ephemeral = True)
 
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
 @tree.command(name="ethics",description="Code of Ethics :3")
 async def ethics(interaction: discord.Interaction):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     ethicsArray = [
         "First of all, love the Lord God with your whole heart, your whole soul, and your whole strength.",
@@ -212,7 +232,9 @@ async def ethics(interaction: discord.Interaction):
         "Never despair of God's mercy."
     ]
     number = random.randint(0, len(ethicsArray) - 1)
-    await interaction.response.send_message(f"{number + 1}. {ethicsArray[number]}")
+    embed = discord.Embed(title = "Ethics", color = myColor)
+    embed.add_field(name = f"{number + 1}.", value = ethicsArray[number])
+    await interaction.response.send_message(embed = embed)
 
 
 @app_commands.allowed_installs(guilds=True, users=True)
@@ -220,44 +242,48 @@ async def ethics(interaction: discord.Interaction):
 @tree.command(name="advice",description="Advice for Budding Streamers")
 async def advice(interaction: discord.Interaction):
     if sus(interaction.user.id):
-        await interaction.response.send_message("impostor >:(", ephemeral = True)
+        embed = discord.Embed(title = "impostor >:(", color = discord.Color.red())
+        embed.set_image(url="https://static.wikia.nocookie.net/mcleodgaming/images/f/fa/Crewmate.png/revision/latest?cb=20230119035540")
+        await interaction.response.send_message(embed = embed, ephemeral = True)
         return
     adviceArray = [
-        "1. Be kind and wholesome; positivity is inherently contagious.",
-        "2. Be yourself, but be measured: people want authenticity, but too much feels like therapy.",
-        "3. Model the behavior you want to see in your followers.",
-        "4. Have an extremely cool penis.",
-        "5. Be famous, sexually-active, and have a large social media following.",
-        "6. Be very famous, have a huge social media following, and be extremely sexually-active.",
-        "7. Dig up a skeleton from the cemetery on stream.",
-        "8. Find a sick child and film him. Viewers appreciate sick children.",
-        "9. Give your dog a math problem and stream him struggling with it. Viewers like to know they're not alone.",
-        "10. Add a laugh track to your stream so that your audience knows when to laugh. This will help remind your audience that they have missed out on a golden opportunity to laugh.",
-        "11.  Never make your streaming schedule too rigid. You want to give yourself an opportunity to take a day off.",
-        "12. Film the future. It will give your viewers on opportunity to make trades from which they can profit handsomely.",
-        "13. Relish the opportunity to correct your own misstatements. It's not a sign of weakness: it gives viewers comfort to know that you're fallible too.",
-        "14. Show your iPad screen when you're playing a mobile game: people love to know that their heroes have iPads.",
-        "15. Let your viewers know that you're (eventually) going to tell them how to get rich off of bitcoin; but to shut the fuck, be quiet, and focus on your stream until you're ready to tell them.",
-        "16. Charge your phone on camera (optional).",
-        "17. The best streamers are even better listeners. Your followers didn't show up to listen to you talk all day — they showed up to listen to you listen.",
-        "18. Bend your viewers to your will.",
-        "19. Never forget your roots. Your stream started out on Justin.tv, which was a platform designed for traditional content distribution. Give a shout out to Justin every once in a while.",
-        "20. Require that users enable Macromedia Flash Player to view your stream.",
-        "21. Share some of your passwords with your audience to establish trust.",
-        "22. You don't have to stream games! Sometimes your viewers just want a crash course on how to build a nuclear bomb, or a history lesson on Syrian military victories.",
-        "23. Don't be afraid to give your viewers a reason to fear and hate you.",
-        "24. Play Russian Roulette on stream. Tell your viewers that you'll add a bullet to the chamber for every donation.",
-        "25. Donate to yourself on stream to show to your audience that you're just as invested in your own success as they are.",
-        "26. Followers remember what they see, and they'll forget the things they never saw.",
-        "27. When it's hot out, make sure to drink water on stream. When it's cold out, make sure to drink hot chocolate. Be consistent.",
-        "28. Don't be afraid to show your belly button and/or asshole.",
-        "29. People who chat the most, donate the least. Eliminate them.",
-        "30. Tell your viewers to hold their applause until the end. Ban anybody who doesn't listen.",
-        "31. Constantly remind your viewers that 'age is just a number.'",
-        "32. Release your complete tax returns and long-form birth certificate on stream so the audience knows you're a red-blooded American."
+        "Be kind and wholesome; positivity is inherently contagious.",
+        "Be yourself, but be measured: people want authenticity, but too much feels like therapy.",
+        "Model the behavior you want to see in your followers.",
+        "Have an extremely cool penis.",
+        "Be famous, sexually-active, and have a large social media following.",
+        "Be very famous, have a huge social media following, and be extremely sexually-active.",
+        "Dig up a skeleton from the cemetery on stream.",
+        "Find a sick child and film him. Viewers appreciate sick children.",
+        "Give your dog a math problem and stream him struggling with it. Viewers like to know they're not alone.",
+        "Add a laugh track to your stream so that your audience knows when to laugh. This will help remind your audience that they have missed out on a golden opportunity to laugh.",
+        "Never make your streaming schedule too rigid. You want to give yourself an opportunity to take a day off.",
+        "Film the future. It will give your viewers on opportunity to make trades from which they can profit handsomely.",
+        "Relish the opportunity to correct your own misstatements. It's not a sign of weakness: it gives viewers comfort to know that you're fallible too.",
+        "Show your iPad screen when you're playing a mobile game: people love to know that their heroes have iPads.",
+        "Let your viewers know that you're (eventually) going to tell them how to get rich off of bitcoin; but to shut the fuck, be quiet, and focus on your stream until you're ready to tell them.",
+        "Charge your phone on camera (optional).",
+        "The best streamers are even better listeners. Your followers didn't show up to listen to you talk all day — they showed up to listen to you listen.",
+        "Bend your viewers to your will.",
+        "Never forget your roots. Your stream started out on Justin.tv, which was a platform designed for traditional content distribution. Give a shout out to Justin every once in a while.",
+        "Require that users enable Macromedia Flash Player to view your stream.",
+        "Share some of your passwords with your audience to establish trust.",
+        "You don't have to stream games! Sometimes your viewers just want a crash course on how to build a nuclear bomb, or a history lesson on Syrian military victories.",
+        "Don't be afraid to give your viewers a reason to fear and hate you.",
+        "Play Russian Roulette on stream. Tell your viewers that you'll add a bullet to the chamber for every donation.",
+        "Donate to yourself on stream to show to your audience that you're just as invested in your own success as they are.",
+        "Followers remember what they see, and they'll forget the things they never saw.",
+        "When it's hot out, make sure to drink water on stream. When it's cold out, make sure to drink hot chocolate. Be consistent.",
+        "Don't be afraid to show your belly button and/or asshole.",
+        "People who chat the most, donate the least. Eliminate them.",
+        "Tell your viewers to hold their applause until the end. Ban anybody who doesn't listen.",
+        "Constantly remind your viewers that 'age is just a number.'",
+        "Release your complete tax returns and long-form birth certificate on stream so the audience knows you're a red-blooded American."
     ]
     number = random.randint(0, len(adviceArray) - 1)
-    await interaction.response.send_message(f"{adviceArray[number]}")
+    embed = discord.Embed(title = "Advice", color = myColor)
+    embed.add_field(name = f"{number + 1}.", value = adviceArray[number])
+    await interaction.response.send_message(embed = embed)
 
 @client.event
 async def on_ready():
